61e90be23aa11d25020d0a44637419cf1db6cb56
Arkadiusz Kruszyński <arkadiusz.h.kruszynski@gmail.com>
2020-03-22 16:14:06 +0100

    Summary of working during this term

--- House_manager_Arduino/__vm/.House_manager_Arduino.vsarduino.h
+++ House_manager_Arduino/__vm/.House_manager_Arduino.vsarduino.h
@@ -1,9 +1,10 @@
 /* 
-	Editor: http://www.visualmicro.com
-			visual micro and the arduino ide ignore this code during compilation. this code is automatically maintained by visualmicro, manual changes to this file will be overwritten
-			the contents of the Visual Micro sketch sub folder can be deleted prior to publishing a project
-			all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
-			note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
+	Editor: https://www.visualmicro.com/
+			This file is for intellisense purpose only.
+			Visual micro (and the arduino ide) ignore this code during compilation. This code is automatically maintained by visualmicro, manual changes to this file will be overwritten
+			The contents of the _vm sub folder can be deleted prior to publishing a project
+			All non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
+			Note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
 	
 	Hardware: Arduino/Genuino Uno, Platform=avr, Package=arduino
 */
@@ -12,13 +13,16 @@
 
 #ifndef _VSARDUINO_H_
 #define _VSARDUINO_H_
-#define __AVR_ATmega328p__
+#define __AVR_atmega328p__
 #define __AVR_ATmega328P__
+#define __AVR_ATmega328p__
+#define _VMDEBUG 1
 #define F_CPU 16000000L
 #define ARDUINO 10805
 #define ARDUINO_AVR_UNO
 #define ARDUINO_ARCH_AVR
 #define __cplusplus 201103L
+#define _Pragma(x)
 #define __AVR__
 #define __inline__
 #define __asm__(...)
@@ -26,9 +30,6 @@
 #define __inline__
 #define __volatile__
 #define GCC_VERSION 40902
-
-#define __cplusplus 201103L
-#undef __cplusplus
 #define __cplusplus 201103L
 
 #define volatile(va_arg) 
@@ -62,14 +63,15 @@ typedef void *__builtin_va_list;
 
 
 
-#include <arduino.h>
+#include "arduino.h"
 #include <pins_arduino.h> 
 //#undef F
 //#define F(string_literal) ((const PROGMEM char *)(string_literal))
 #undef PSTR
 #define PSTR(string_literal) ((const PROGMEM char *)(string_literal))
 
-typedef unsigned char uint8_t;
+//typedef unsigned char uint8_t;
+//typedef unsigned int uint8_t;
 
 #define pgm_read_byte(address_short) uint8_t() 
 #define pgm_read_word(address_short) uint16_t() 
